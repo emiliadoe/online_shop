63ab967b23a5785875b75c2e43fd9419debd8fc8
emiliadoe <emilia.d@web.de>
2024-07-08 20:30:00 +0300

    homepage for customer service added

--- django_shop/CustomerService/urls.py
+++ django_shop/CustomerService/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='homeCustomerService.html'), name='homeCustomerService'),
     path('products/add/', views.ProductAddView.as_view(), name='add-product'),
     path('products/edit/<int:pk>/', views.ProductEditView.as_view(), name='edit-product'),
     path('delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
