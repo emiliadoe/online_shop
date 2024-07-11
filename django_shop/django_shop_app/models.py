c9f8ffbe486677c8ba75eacc10e55ee40d2472da
emiliadoe <emilia.d@web.de>
2024-07-11 19:39:54 +0300

    content added to DB

--- django_shop/django_shop_app/models.py
+++ django_shop/django_shop_app/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
 
     price= models.DecimalField(max_digits=10, decimal_places=2)
 
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
 
     image = models.ImageField(upload_to='product_images', blank=True, null=True)
 
